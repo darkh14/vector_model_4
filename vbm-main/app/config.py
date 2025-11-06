907ffbe99fbdf8fba68b85f155d6cab94274ea6b
darkh14 <dark_h14@mail.ru>
2025-11-06 17:39:32 +0300

    change version

--- vbm-main/app/config.py
+++ vbm-main/app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 from pathlib import Path
 import os
 
-VERSION = '4.0.1.2'
+VERSION = '4.0.2.1'
 BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class Settings(BaseSettings):
